7fa838395c85e3371a21895d1af92dd39921f82a
Zakaria Hachm <122728271+zakaria68K@users.noreply.github.com>
2025-03-31 13:21:09 +0200

    Update TransformationManager.java

--- app/src/main/java/fr/imta/naomod/atl/TransformationManager.java
+++ app/src/main/java/fr/imta/naomod/atl/TransformationManager.java
@@ -33,7 +33,7 @@ public class TransformationManager {
         List<File> dirsToProcess = new ArrayList<>();
 
         // Add original transformations directory
-        File originalDir = new File("/home/goldensuneur/work/atl_zoo");
+        File originalDir = new File("./path//to/youDirectory");//put your atl zoo project directory
         if (originalDir.exists()) {
             dirsToProcess.add(originalDir);
         }
